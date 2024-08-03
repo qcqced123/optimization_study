8821b1c58c043d72547c8d222acf5e1e947693e5
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-08-03 18:21:30 +0900

    Update prompt_maker.py

--- optimization/prompt/prompt_maker.py
+++ optimization/prompt/prompt_maker.py
@@ -64,7 +64,7 @@ Question 3: What is PagedAttention, and how does it differ from traditional atte
 Your Task: For the given text from Context 4, generate questions based on the specific guidelines provided.
 Generate appropriate questions according to the given context from Context 4 only.
 Keep the number of questions between 1 and 3.
-ㄴCreate only the question text.
+Create only the question text.
 Separate the questions you want to create with newlines.
 
 Context 4: {context}
