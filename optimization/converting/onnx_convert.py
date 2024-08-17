ece2ee06a3b792896f69559088b2cb882e3002da
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-08-17 10:35:55 +0900

    Update onnx_convert.py

--- optimization/converting/onnx_convert.py
+++ optimization/converting/onnx_convert.py
@@ -6,39 +6,49 @@ from transformers import pipeline, BitsAndBytesConfig
 from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM
 
 
-""" load the pretrained config, tokenizer, model from huggingface hub """
+def get_config(name: str) -> AutoConfig:
+    return AutoConfig.from_pretrained(
+        pretrained_model_name_or_path=name,
+        trust_remote_code=True
+    )
 
-model_name = "microsoft/Phi-3-mini-128k-instruct"
-config = AutoConfig.from_pretrained(
-    pretrained_model_name_or_path=model_name,
-    trust_remote_code=True
-)
-bit_config = None
-tokenizer = AutoTokenizer.from_pretrained(
-    pretrained_model_name_or_path=model_name,
-    trust_remote_code=True
-)
-model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path=model_name,
-    config=config,
-    quantization_config=bit_config,
-    # attn_implementation="flash_attention_2",
-    trust_remote_code=True,
-    torch_dtype="auto"
-)
 
-""" convert pytorch model to onnx """
+def get_tokenizer(name: str) -> AutoTokenizer:
+    return AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=name,
+        trust_remote_code=True
+    )
 
-main_export(
-    model_name_or_path=model_name,
-    config=config,
-    output="./saved/",
-    task="text-generation-with-past",
-    #opset=20,
-    device="cpu",
-    dtype="bf16",
-    optimize="O3",
-    framework="pt",
-    trust_remote_code=True,
-)
 
+def get_model(name: str, config: AutoConfig, bit_config: BitsAndBytesConfig = None) -> AutoModelForCausalLM:
+    return AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=name,
+        config=config,
+        quantization_config=bit_config,
+        attn_implementation="flash_attention_2",
+        trust_remote_code=True,
+        torch_dtype="auto"
+    )
+
+
+def convert2onnx(name: str, config: AutoConfig) -> None:
+    main_export(
+        model_name_or_path=name,
+        config=config,
+        output="./saved/",
+        task="text-generation-with-past",
+        opset=21,
+        device="cpu",
+        dtype="bf16",
+        optimize="O3",
+        framework="pt",
+        trust_remote_code=True,
+    )
+
+
+if __name__ == '__main__':
+    model_name = "microsoft/Phi-3-mini-128k-instruct"
+    config = get_config(model_name)
+    tokenizer = get_tokenizer(model_name)
+    model = get_model(model_name, config)
+    convert2onnx(model_name, config)
