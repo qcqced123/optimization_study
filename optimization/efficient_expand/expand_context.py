21d8b0923037b5b5078d9dfae33fb739ea1d7a4d
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-08-25 16:25:36 +0900

    Update expand_context.py

--- optimization/efficient_expand/expand_context.py
+++ optimization/efficient_expand/expand_context.py
@@ -68,6 +68,14 @@ def expand_pos_emb(pos_emb: nn.Embedding, target_size: int) -> nn.Embedding:
     return new_emb
 
 
+def save_model(model: nn.Module, config: AutoConfig, tokenizer: AutoTokenizer, to: str) -> None:
+    """ save the current state of model's config, tokenizer config """
+    print(f"Saving bf16 model to {to}...")
+    model.save_pretrained(to)
+    tokenizer.save_pretrained(to)
+    config.save_pretrained(to)
+
+
 if __name__ == '__main__':
     # usage example code
     # filtering function for getting more good quality of data
@@ -108,33 +116,35 @@ if __name__ == '__main__':
         torch.zeros(model.embeddings.position_ids.size(), dtype=torch.long)
     )  # if you use bert
 
-    text = """Lanckriet, 2007
-    features is large. This emphasises SCCA’s ability to learn the semantic space from a small number of relevant features.
-    In Section 2] we give a brief review of CCA, and Section [3] formulates and de- fines SCCA. In Section [4] we derive our optimisation problem and show how all the pieces are assembled to give the complete algorithm. The experiments on the paired bilingual data-sets are given in Section 5] Section [6] concludes this paper.
-
-    2 Canonical Correlation Analysis
-    We briefly review canonical correlation analysis and its ML-dual (kernel) variant to provide a smooth understanding of the transition to the sparse formulation. First, basic notation representation used in the paper is defined
-    b —_ boldface lower case letters represent vectors
-    s — lower case letters represent scalars
-    M -— _ upper case letters represent matrices.
-    The correlation between x, and x, can be computed as
-    where Cag = XoX/, and Cy, = X,Xj are the within-set covariance matrices and Cy, = X_Xj is the between-sets covariance matrix, X, is the matrix whose columns are the vectors x;, i = 1,..., from the first representation while X;, is the matrix with columns x; from the second representation. We are able to observe that scaling w,, w, does not effect the quotient in equa- tion (i), which is therefore equivalent to maximising w/,C,,wy subject to WwW CaaWa = W,CwWs = 1.
-    The kernelising of CCA (Fyfe & Lai, 2000a} Fyfe & Lai, 2000b) offers an al- ternative by first projecting the data into a higher dimensional feature space dt 2 X = (41,..-,%n) > O:(x) = (G1(x),..., Ow(x)) (N > n,t =a,b) be fore performing CCA in the new feature spaces. The kernel variant of CCA
-    is useful when the correlation is believed to exist in some non linear relation- ship. Given the kernel functions kK, and ky let Ka = X/Xq and Ky = X},Xp be the linear kernel matrices corresponding to the two representations of the data, where X, is now the matrix whose columns are the vectors a(xi), i=1,...,¢from the first representation while X; is the matrix with columns o»(x:) from the second representation. The weights w, and wy, can be ex- pressed as a linear combination of the training examples wz = X,a and w, = X,3. Substitution into the ML-primal CCA equation (f) gives the optimisation
-    which is equivalent to maximising a’ K,K,B subject to a’ K2a = B'K2B = 1. This is the ML-dual form of the CCA optimisation problem given in equa- tion (I) which can be cast as a generalised eigenvalue problem and for which the first & generalised eigenvectors can be found efficiently. Both CCA and KCCA can be formulated as symmetric eigenproblems.
-    A variety of theoretical analyses have been presented for CCA (Akaho, 2001} A common conclusion of some of these analyses is the need to regularise KCCA. For example the quality of the generalisation of the associated pat- tern function is shown to be controlled by the sum of the squares of the weight vector norms in (Hardoon & Shawe-Taylor, In Press). Although there are advantages in using KCCA, which have been demonstrated in various ex- periments across the literature, we clarify that when using a linear kernel in both views, regularised KCCA is the same as regularised CCA (since the former and latter are linear). Nonetheless using KCCA with a linear kernel can have advantages over CCA, the most important being speed when the number of features is larger than the number of samples|}|
-
-    3 Sparse CCA
-    The motivation for formulating a ML primal-dual SCCA is largely intuitive when faced with real-world problems combined with the need to understand or interpret the found solutions. Consider the following examples as potential
-
-    Bach"""
-    inputs = tokenizer(text, return_tensors="pt")
-
-    feature = model(
-        input_ids=inputs["input_ids"],
-        token_type_ids=inputs["token_type_ids"],
-        attention_mask=inputs["attention_mask"]
-    )
-    last_hidden_state, cls_pooling = feature["last_hidden_state"], feature["pooler_output"]
-    print(f"shape of last hidden state from expanded max_sequence length model is: {last_hidden_state.shape}")
-    print(f"shape of cls pooling output from expanded max_sequence length model is: {cls_pooling.shape}")
+    # text = """Lanckriet, 2007
+    # features is large. This emphasises SCCA’s ability to learn the semantic space from a small number of relevant features.
+    # In Section 2] we give a brief review of CCA, and Section [3] formulates and de- fines SCCA. In Section [4] we derive our optimisation problem and show how all the pieces are assembled to give the complete algorithm. The experiments on the paired bilingual data-sets are given in Section 5] Section [6] concludes this paper.
+    #
+    # 2 Canonical Correlation Analysis
+    # We briefly review canonical correlation analysis and its ML-dual (kernel) variant to provide a smooth understanding of the transition to the sparse formulation. First, basic notation representation used in the paper is defined
+    # b —_ boldface lower case letters represent vectors
+    # s — lower case letters represent scalars
+    # M -— _ upper case letters represent matrices.
+    # The correlation between x, and x, can be computed as
+    # where Cag = XoX/, and Cy, = X,Xj are the within-set covariance matrices and Cy, = X_Xj is the between-sets covariance matrix, X, is the matrix whose columns are the vectors x;, i = 1,..., from the first representation while X;, is the matrix with columns x; from the second representation. We are able to observe that scaling w,, w, does not effect the quotient in equa- tion (i), which is therefore equivalent to maximising w/,C,,wy subject to WwW CaaWa = W,CwWs = 1.
+    # The kernelising of CCA (Fyfe & Lai, 2000a} Fyfe & Lai, 2000b) offers an al- ternative by first projecting the data into a higher dimensional feature space dt 2 X = (41,..-,%n) > O:(x) = (G1(x),..., Ow(x)) (N > n,t =a,b) be fore performing CCA in the new feature spaces. The kernel variant of CCA
+    # is useful when the correlation is believed to exist in some non linear relation- ship. Given the kernel functions kK, and ky let Ka = X/Xq and Ky = X},Xp be the linear kernel matrices corresponding to the two representations of the data, where X, is now the matrix whose columns are the vectors a(xi), i=1,...,¢from the first representation while X; is the matrix with columns o»(x:) from the second representation. The weights w, and wy, can be ex- pressed as a linear combination of the training examples wz = X,a and w, = X,3. Substitution into the ML-primal CCA equation (f) gives the optimisation
+    # which is equivalent to maximising a’ K,K,B subject to a’ K2a = B'K2B = 1. This is the ML-dual form of the CCA optimisation problem given in equa- tion (I) which can be cast as a generalised eigenvalue problem and for which the first & generalised eigenvectors can be found efficiently. Both CCA and KCCA can be formulated as symmetric eigenproblems.
+    # A variety of theoretical analyses have been presented for CCA (Akaho, 2001} A common conclusion of some of these analyses is the need to regularise KCCA. For example the quality of the generalisation of the associated pat- tern function is shown to be controlled by the sum of the squares of the weight vector norms in (Hardoon & Shawe-Taylor, In Press). Although there are advantages in using KCCA, which have been demonstrated in various ex- periments across the literature, we clarify that when using a linear kernel in both views, regularised KCCA is the same as regularised CCA (since the former and latter are linear). Nonetheless using KCCA with a linear kernel can have advantages over CCA, the most important being speed when the number of features is larger than the number of samples|}|
+    #
+    # 3 Sparse CCA
+    # The motivation for formulating a ML primal-dual SCCA is largely intuitive when faced with real-world problems combined with the need to understand or interpret the found solutions. Consider the following examples as potential
+    #
+    # Bach"""
+    # inputs = tokenizer(text, return_tensors="pt")
+    #
+    # feature = model(
+    #     input_ids=inputs["input_ids"],
+    #     token_type_ids=inputs["token_type_ids"],
+    #     attention_mask=inputs["attention_mask"]
+    # )
+    # last_hidden_state, cls_pooling = feature["last_hidden_state"], feature["pooler_output"]
+    # print(f"shape of last hidden state from expanded max_sequence length model is: {last_hidden_state.shape}")
+    # print(f"shape of cls pooling output from expanded max_sequence length model is: {cls_pooling.shape}")
+    config.max_position_embeddings = expand_size
+    save_model(model, config, tokenizer, f"saved/{model_name.split('/')[-1]}/")
