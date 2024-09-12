31e44c001fbe8e9697aa850a6cbf3185e25cdf1e
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-09-12 19:37:32 +0900

    Update ort_quantization.py

--- optimization/quantization/ort_quantization.py
+++ optimization/quantization/ort_quantization.py
@@ -2,10 +2,27 @@ import os
 import onnx
 import numpy as np
 
+from typing import List
 from tqdm.auto import tqdm
 from transformers import AutoConfig, AutoTokenizer
-from onnx import helper, numpy_helper, TensorProto
+from onnx import helper, numpy_helper, TensorProto, ModelProto
 from onnxruntime.quantization import matmul_4bits_quantizer, matmul_bnb4_quantizer
+
+
+def check_initializer(onnx_model: ModelProto) -> None:
+    """ helper function for printing current input model's initializer(weight) name, dtype"""
+    for initializer in tqdm(onnx_model.graph.initializer):
+        name = initializer.name
+        data_type = initializer.data_type
+        data_type_str = TensorProto.DataType.Name(data_type)
+        print(f"ORT model weight name: {name}, Data Type: {data_type_str}", end="\n")
+
+
+def check_node(onnx_model: ModelProto) -> None:
+    """ helper function for printing current input model's node name"""
+    for node in tqdm(onnx_model.graph.node):
+        name = node.name
+        print(f"ORT model node name: {name}", end="\n")
 
 
 def int8_quantize(model_name: str, target_weight_name: str):
@@ -110,30 +127,37 @@ def fp32_to_bf16(fp32_array: np.ndarray) -> np.ndarray:
 
 
 def bfloat16_quantize(
-        model_name: str,
-        target_weight_name: str = "model.embed_tokens.weight",
-        bf16_model_path: str = ""
+    model_name: str,
+    target_weight_name: List[str] = None,
+    is_tied: bool = False,
+    bf16_model_path: str = None
 ) -> None:
     """ fp32 node-wise, initializer-wise(weight-wise) quantize to bf16 function
 
     bf16 is alias of bfloat16 or brain float16, made by Google 2019 for targeting Deep Learning
     ONNX support bf16, but currently some of operators such as "where" already contained several famous LLM in ONNX does not support bf16
 
-    So, we cannot export the pytorch or tensorflow model into bf16 directly, left options are fp32, fp16
+    so, we cannot export the pytorch or tensorflow model into bf16 directly, left options are fp32, fp16
     ONNX fp16 does not support the CPU accelerator, when you use CPU accelerator, just only one option left (fp32)
 
-    Many Quantization options or functions are already implemented in ONNX Library, but they do not quantize some module
+    many quantization options or functions are already implemented in ONNX Library, but they do not quantize some module
     such as word embedding layer.
 
-    So if you want to bf16 or other float precision of word embedding layer, you set up the precision to this layer manually
+    so if you want to bf16 or other float precision of word embedding layer, you set up the precision to this layer manually
     this function will do that action exactly, and also have de-cast bf16 to fp32 node(operator) for weight-shared model
     (weight-shared & tied-embedding: word embedding's weight and lm head weight are the same and referencing each other)
 
     Args:
-        model_name (str): path of local model hub
-        target_weight_name (str): name of weight name to apply converting fp32 to bf16 for reducing model storage
-                                  default value is model.embed_tokens.weight, indicating word embedding layers weight
+        model_name (str): path of local model hub,
+
+        target_weight_name (str):
+            name of weight name to apply converting fp32 to bf16 for reducing model storage,
+            default value is model.embed_tokens.weight, indicating word embedding layers weight
+
+        is_tied (bool): flag value of sharing embedding weight option from input model
+
         bf16_model_path (str): path of local model hub to save the bf16 model
+
     Reference:
         https://onnx.ai/onnx/
         https://onnxruntime.ai/docs/
@@ -141,13 +165,15 @@ def bfloat16_quantize(
     # load onnx format model from local model hub
     # find the target node's weight to converting fp32 to bf16
     # convert fp32 weight to bf16
-    onnx_model = onnx.load(model_name)
+    checkpoint_name = model_name.split("/")[2].split("-")[0]  # get quantized information from file name
+    onnx_model = onnx.load(f"{model_name}{checkpoint_name}_model.onnx")
+
     for initializer in tqdm(onnx_model.graph.initializer):
         name = initializer.name
         data_type = initializer.data_type
         data_type_str = TensorProto.DataType.Name(data_type)
 
-        if name == target_weight_name:
+        if name in target_weight_name:
             print(f"ORT model weight name: {name}, Data Type: {data_type_str}", end="\n\n")
 
             # export target onnx module's weight into numpy ndarray
@@ -164,45 +190,48 @@ def bfloat16_quantize(
             onnx_model.graph.initializer.remove(initializer)
             onnx_model.graph.initializer.append(bf16_initializer)
 
-    # initialize the name of target node
-    # initialize the name of target node's weight name
-    target_node_name = "Transpose_5369"
-    target_tensor_name = target_weight_name
+    # branch for "is_tied" = True
+    if is_tied:
+        # initialize the name of target node
+        # initialize the name of target node's weight name
+        target_node_name = "Transpose_5369"
+        target_tensor_name = target_weight_name
 
-    # initialize the de-cast layer's output name
-    cast_node_output_name = target_tensor_name + "_fp32"
-    cast_node = onnx.helper.make_node(
-        "Cast",
-        inputs=[target_tensor_name],  # define the input: bf16 weight
-        outputs=[cast_node_output_name],  # define the output: fp32 weight
-        to=onnx.TensorProto.FLOAT,  # define the target dtype, (FLOAT = fp32)
-        name="Cast_BF16_to_FP32"
-    )
+        # initialize the de-cast layer's output name
+        cast_node_output_name = target_tensor_name + "_fp32"
+        cast_node = onnx.helper.make_node(
+            "Cast",
+            inputs=[target_tensor_name],  # define the input: bf16 weight
+            outputs=[cast_node_output_name],  # define the output: fp32 weight
+            to=onnx.TensorProto.FLOAT,  # define the target dtype, (FLOAT = fp32)
+            name="Cast_BF16_to_FP32"
+        )
 
-    # make the new graph node, including the de-cast node
-    # add the de-cast node into new graph node
-    # remove the old graph node
-    # add the new graph node, including the de-cast node
-    # save the new graph model in local model hub
-    new_nodes = []
-    for node in tqdm(onnx_model.graph.node):
-        if node.name == target_node_name:
-            new_inputs = [cast_node_output_name if inp == target_tensor_name else inp for inp in node.input]
-            modified_node = onnx.helper.make_node(
-                node.op_type,
-                inputs=new_inputs,
-                outputs=node.output,
-                name=node.name
-            )
-            new_nodes.append(modified_node)
-        else:
-            new_nodes.append(node)
+        # make the new graph node, including the de-cast node
+        # add the de-cast node into new graph node
+        # remove the old graph node
+        # add the new graph node, including the de-cast node
+        # save the new graph model in local model hub
+        new_nodes = []
+        for node in tqdm(onnx_model.graph.node):
+            if node.name == target_node_name:
+                new_inputs = [cast_node_output_name if inp == target_tensor_name else inp for inp in node.input]
+                modified_node = onnx.helper.make_node(
+                    node.op_type,
+                    inputs=new_inputs,
+                    outputs=node.output,
+                    name=node.name
+                )
+                new_nodes.append(modified_node)
+            else:
+                new_nodes.append(node)
 
-    new_nodes.insert(0, cast_node)
-    onnx_model.graph.ClearField("node")
-    onnx_model.graph.node.extend(new_nodes)
-    onnx.save(onnx_model, bf16_model_path)
+        new_nodes.insert(0, cast_node)
+        onnx_model.graph.ClearField("node")
+        onnx_model.graph.node.extend(new_nodes)
 
+    # save the converted model to local disk
+    onnx.save(onnx_model, f"{bf16_model_path}{checkpoint_name}_model.onnx")
     return
 
 
@@ -217,12 +246,17 @@ def rtn_4bit_quantize(
     This algorithm doesn't quantize the word embedding, language modeling head
 
     if you want to quantize those modules, you must do manually
+
+
     Args:
         model_name (str):
         output_path (str):
         block_size (int):
         accuracy_level (int):
         algo_config:
+
+    References:
+        https://github.com/microsoft/onnxruntime/blob/main/docs/ContribOperators.md#commicrosoftmatmulnbits
     """
     ort_model = onnx.load(f"{model_name}model.onnx")
     ort_config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
@@ -256,7 +290,7 @@ def qlora_4bit_quantize(
     This algorithm doesn't quantize the word embedding, language modeling head
 
     if you want to quantize those modules, you must do manually
-    
+
     Args:
         model_name (str):
         quant_type (str):
@@ -284,13 +318,25 @@ def qlora_4bit_quantize(
 
 
 if __name__ == '__main__':
-    model_name = "./saved/onnx-stage5-eeve-phi3.5-mini-instruct/"
-    output_path = "./saved/W4A16-onnx-stage5-eeve-phi3.5-mini-instruct/"
-    q_config = matmul_4bits_quantizer.RTNWeightOnlyQuantConfig()
-    rtn_4bit_quantize(
-        model_name=model_name,
-        output_path=output_path,
-        block_size=128,
-        accuracy_level=4,
-        algo_config=q_config
-    )
+    # model_name = "./saved/onnx-stage5-eeve-phi3.5-mini-instruct/"
+    # output_path = "./saved/W4A16-onnx-stage5-eeve-phi3.5-mini-instruct/"
+    # q_config = matmul_4bits_quantizer.RTNWeightOnlyQuantConfig()
+    # rtn_4bit_quantize(
+    #     model_name=model_name,
+    #     output_path=output_path,
+    #     block_size=128,
+    #     accuracy_level=4,
+    #     algo_config=q_config
+    # )
+    model_name = "./saved/W4A16-onnx-stage5-eeve-phi3.5-mini-instruct/W4A16_model.onnx"
+    output_path = "./saved/W4A16E16-onnx-stage5-eeve-phi3.5-mini-instruct/"
+    target_nodes = ["model.embed_tokens.weight", ]
+
+    model = onnx.load(model_name)
+    check_node(model)
+    # bfloat16_quantize(
+    #     model_name=model_name,
+    #     target_weight_name=target_nodes,
+    #     is_tied=False,
+    #     bf16_model_path=output_path
+    # )
