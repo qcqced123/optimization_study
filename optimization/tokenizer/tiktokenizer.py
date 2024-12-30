ef2c1c82814393964c85962cc12500207310aa44
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-12-30 16:08:42 +0900

    Create tiktokenizer.py

---
+++ optimization/tokenizer/tiktokenizer.py
@@ -0,0 +1,5 @@
+""" py module for implementing the tik-tokenizer, originally from OpenAi
+reference:
+    - https://github.com/openai/tiktoken
+    - https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+"""
