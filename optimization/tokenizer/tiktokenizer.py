f4bea7e98502345a01ac110eb082b52bca0ce667
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-12-30 16:39:36 +0900

    Update tiktokenizer.py

--- optimization/tokenizer/tiktokenizer.py
+++ optimization/tokenizer/tiktokenizer.py
@@ -1,5 +1,7 @@
 """ py module for implementing the tik-tokenizer, originally from OpenAi
 reference:
     - https://github.com/openai/tiktoken
+    - https://platform.openai.com/tokenizer
     - https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
 """
+import tiktoken
